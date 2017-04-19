0c664080ac64774210b03dce8471aaa5f4dc5f98
Abimbola Olaitan <craftwordltd@gmail.com>
2017-04-19 14:28:56 +0100

    modify package.json and add app.js, tweet.json for storage

---
+++ app.js
@@ -0,0 +1,6 @@
+// declare dependency variables 
+
+var Twitter = require('twitter');
+var readline = require('readline');
+var jsonfile = require('jsonfile');
+var ProgressBar = require('progress');
